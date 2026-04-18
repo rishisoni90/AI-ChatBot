06890ee24649795addbf51fee9bceaa5fceb2f6f
RISHI SONI <55064658+rishisoni90@users.noreply.github.com>
2026-04-18 18:50:20 -0400

    Update llm

--- llm.py
+++ llm.py
@@ -1,9 +1,13 @@
+import streamlit as st
 from groq import Groq
 
 def call_llm(messages):
-    client = Groq(api_key="your_groq_api_key_here")  # paste your key
+    # filter out system messages if needed
+    filtered = [m for m in messages if m["role"] in ["user", "assistant"]]
+    
+    client = Groq(api_key=st.secrets["GROQ_API_KEY"])
     response = client.chat.completions.create(
         model="llama3-8b-8192",
-        messages=messages
+        messages=filtered
     )
     return response.choices[0].message.content
